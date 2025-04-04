4dabaadd54fb28e390b48a5a3205aa539efacd7f
Harshtank123 <161057194+Harshtank123@users.noreply.github.com>
2025-04-04 11:47:15 +0530

    Create PR2.java

---
+++ PR2.java
@@ -0,0 +1,14 @@
+public class Main{
+      public static void main(String[] args) {
+         int h = 20;
+         int i = 10;
+        int j = 40;
+        if(h > i && h > j){
+            System.out.println("Maximum Number : " + h);
+        }else if(i > h && i > j){
+             System.out.println("maximum Number: " + i);
+        }else{
+             System.out.println("Maximum number: " + j);
+         }
+      }   
+    }
