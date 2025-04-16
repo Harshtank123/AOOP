a512637a8f6f90fcbbb2ed0f15be74dfb341b0d3
Harshtank123 <161057194+Harshtank123@users.noreply.github.com>
2025-04-15 23:50:16 -0700

    Update

---
+++ PR21.java
@@ -0,0 +1,11 @@
+final class d1{
+    public void r1(){
+        System.out.println("final class");
+    }
+}
+public class main{
+    public static void main(String[] args) {
+        d1 obj=new d1();
+        obj.r1();
+    }
+}
